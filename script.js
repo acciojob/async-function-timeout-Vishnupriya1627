5508b598f5f499e663827011efdee9a8a6ff7530
Vishnupriya1627 <mamillapallivishnupriya@gmail.com>
2026-05-27 13:31:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+const text = document.querySelector("#text");
+const delay = document.querySelector("#delay");
+const btn = document.querySelector("#btn");
+const output = document.querySelector("#output");
+
+const promise1 = async () => {
+  await new Promise((resolve, reject) => {
+    setTimeout(() => {
+      output.innerHTML = text.value;
+      resolve();
+    }, delay.value);
+  });
+  return "promise resolved";
+};
+
+btn.addEventListener("click", promise1);
